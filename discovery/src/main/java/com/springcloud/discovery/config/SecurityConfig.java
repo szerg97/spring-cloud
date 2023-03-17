13d21e29717947713203bc8899ed4af010402f8e
Gergo Szalai <szerg97@gmail.com>
2023-03-17 11:20:42 +0100

    configured discovery server

--- discovery/src/main/java/com/springcloud/discovery/config/SecurityConfig.java
+++ discovery/src/main/java/com/springcloud/discovery/config/SecurityConfig.java
@@ -1,4 +1,34 @@
 package com.springcloud.discovery.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@Order(1)
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+                .and()
+            .authorizeHttpRequests()
+                .requestMatchers("/eureka/**")
+                .hasRole("SYSTEM")
+                .anyRequest()
+                .denyAll()
+                .and()
+            .httpBasic()
+                .and()
+            .csrf()
+                .disable();
+        return http.build();
+    }
 }
